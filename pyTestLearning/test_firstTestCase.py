2079ff8e312afadca7abaf9441dc67c3c49948fc
Eddie Saunders <saunders.eddie@outlook.com>
2024-08-05 06:03:26 -0400

    Added Pytest, configured VS Code test environment, executed a few tests

---
+++ pyTestLearning/test_firstTestCase.py
@@ -0,0 +1,15 @@
+# import pytest
+
+def test_doLogin():
+    print("Executing Login Test")
+    a = 3
+    b = 4
+    assert a + 1 == b, "Test failed"
+    assert a == b, "Test failed"
+
+def test_testingUserReq():
+    print("Testing User Registration")
+    a = 3
+    b = 4
+    assert a + 1 == b, "Test failed"
+    assert a == b, "Test failed"
